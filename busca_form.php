beac62292b2db229b4822477b041ce4acbd7d61c
Felipe Braga <fbobraga@gmail.com>
2022-03-24 10:25:03 -0300

    tinha um "/" errado...

--- busca_form.php
+++ busca_form.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form action="/busca_resultado.php">
+<form action="busca_resultado.php">
   <label for="fname">Texto de busca nas transcrições dos episódios:</label><br>
   <input type="text" id="texto_busca" name="texto_busca"><br>
   <input type="submit" value="Buscar">
