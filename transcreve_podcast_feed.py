bb01720e17601a5971c4b5c7310aafc2e051d0e9
Felipe Braga <fbobraga@gmail.com>
2022-03-30 13:10:48 -0300

    arquivo de mídia temporário ter título do episódio

--- transcreve_podcast_feed.py
+++ transcreve_podcast_feed.py
@@ -71,16 +71,16 @@ try:
         if not os.path.exists('{}/{}'.format(sys.argv[2], nome_arq_saida)):
             print('Fazendo download...')
             r = requests.get(i['links'][1]['href'])
-            open('/tmp/arq_media_podcast.{}'.format(sufixo_nome_arq), 'wb').write(r.content)
+            open('/tmp/arq_media_podcast.{}'.format(nome_arq_saida), 'wb').write(r.content)
             print('Processando...')
             process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
-                                        '/tmp/arq_media_podcast.{}'.format(sys.argv[2].replace('/', '_')),
+                                        '/tmp/arq_media_podcast.{}'.format(nome_arq_saida),
                                         '-ar', str(sample_rate) , '-ac', '1', '-f', 's16le', '-'],
                                         stdout=subprocess.PIPE)
 
             arq_saida = open('{}/{}'.format(sys.argv[2], nome_arq_saida), 'w')
             arq_saida.write(srt.compose(transcribe()))
             arq_saida.close()
-            os.remove('/tmp/arq_media_podcast.{}'.format(sufixo_nome_arq))
+            os.remove('/tmp/arq_media_podcast.{}'.format(nome_arq_saida))
 finally:
     os.remove('/tmp/semaforo_transcreve_podcast_feed.{}'.format(sufixo_nome_arq))
