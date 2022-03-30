a81e3632efef70659cc548846108ffd2c646833d
Felipe Braga <fbobraga@gmail.com>
2022-03-30 07:46:43 -0300

    Usando try ... finally (pra sempre apagar o semáforo)

--- transcreve_podcast_feed.py
+++ transcreve_podcast_feed.py
@@ -49,37 +49,38 @@ def transcribe():
            subs.append(s)
     return subs
 
-sample_rate=16000
-model = Model("model")
-rec = KaldiRecognizer(model, sample_rate)
-rec.SetWords(True)
-sufixo_nome_arq = sys.argv[2].replace('/', '_')
+try:
+    sample_rate=16000
+    model = Model("model")
+    rec = KaldiRecognizer(model, sample_rate)
+    rec.SetWords(True)
+    sufixo_nome_arq = sys.argv[2].replace('/', '_')
 
-if os.path.exists('/tmp/semaforo_transcreve_podcast_feed.{}'.format(sufixo_nome_arq)):
-    print ('já em processamento')
-    exit (1)
-arq_semaforo = open('/tmp/semaforo_transcreve_podcast_feed.{}'.format(sufixo_nome_arq), 'w')
-arq_semaforo.write('em execução')
-arq_semaforo.close()
+    if os.path.exists('/tmp/semaforo_transcreve_podcast_feed.{}'.format(sufixo_nome_arq)):
+        print ('já em processamento')
+        exit (1)
+    arq_semaforo = open('/tmp/semaforo_transcreve_podcast_feed.{}'.format(sufixo_nome_arq), 'w')
+    arq_semaforo.write('em execução')
+    arq_semaforo.close()
 
-feed = feedparser.parse(sys.argv[1])
+    feed = feedparser.parse(sys.argv[1])
 
-for i in feed.entries:
-    print('Tratando "{}"...'.format(i['title']))
-    nome_arq_saida = i['title'].replace('/', '_')
-    if not os.path.exists('{}/{}'.format(sys.argv[2], nome_arq_saida)):
-        print('Fazendo download...')
-        r = requests.get(i['links'][1]['href'])
-        open('/tmp/arq_media_podcast.{}'.format(sufixo_nome_arq), 'wb').write(r.content)
-        print('Processando...')
-        process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
-                                    '/tmp/arq_media_podcast.{}'.format(sys.argv[2].replace('/', '_')),
-                                    '-ar', str(sample_rate) , '-ac', '1', '-f', 's16le', '-'],
-                                    stdout=subprocess.PIPE)
+    for i in feed.entries:
+        print('Tratando "{}"...'.format(i['title']))
+        nome_arq_saida = i['title'].replace('/', '_')
+        if not os.path.exists('{}/{}'.format(sys.argv[2], nome_arq_saida)):
+            print('Fazendo download...')
+            r = requests.get(i['links'][1]['href'])
+            open('/tmp/arq_media_podcast.{}'.format(sufixo_nome_arq), 'wb').write(r.content)
+            print('Processando...')
+            process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
+                                        '/tmp/arq_media_podcast.{}'.format(sys.argv[2].replace('/', '_')),
+                                        '-ar', str(sample_rate) , '-ac', '1', '-f', 's16le', '-'],
+                                        stdout=subprocess.PIPE)
 
-        arq_saida = open('{}/{}'.format(sys.argv[2].replace('/', '_'), nome_arq_saida), 'w')
-        arq_saida.write(srt.compose(transcribe()))
-        arq_saida.close()
-        os.remove('/tmp/arq_media_podcast.{}'.format(sufixo_nome_arq))
-
-os.remove('/tmp/semaforo_transcreve_podcast_feed.{}'.format(sufixo_nome_arq))
+            arq_saida = open('{}/{}'.format(sys.argv[2], nome_arq_saida), 'w')
+            arq_saida.write(srt.compose(transcribe()))
+            arq_saida.close()
+            os.remove('/tmp/arq_media_podcast.{}'.format(sufixo_nome_arq))
+finally:
+    os.remove('/tmp/semaforo_transcreve_podcast_feed.{}'.format(sufixo_nome_arq))
